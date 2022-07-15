58250f8303a0bef3caeeee202c49e4f1be684ec5
kjg9812 <kjgarcia327@gmail.com>
2022-07-15 13:58:08 -0700

    button updates

--- scripts.js
+++ scripts.js
@@ -1,28 +1,53 @@
-function highlight(element){
-  console.log("changing")
-  isClicked = !isClicked;
-  element.style = (isClicked == true) ? "background-color: white; color:rgb(255, 200, 99)" : "background-color: rgb(255, 200, 99);color:white";
+//onclick style change for all buttons
+function highlight(element,name,clicked){
+  console.log("changing");
+  oppclicked = !clicked;
+  window[name] = oppclicked
+  element.style = (oppclicked == true) ? "background-color: white; color:rgb(255, 200, 99)" : "background-color: rgb(255, 200, 99);color:white"
 }
 
-var element = document.getElementById("bananas")
-var isClicked = false;
-element.onclick = () => {
-  highlight(element)
-  if (isClicked === true){
-    sendApiRequest("bananas")
+//String for string builder
+let foodString = []
+
+//buttons
+let bananas = document.getElementById("bananas")
+let bananaName = 'bananaClicked';
+var bananaClicked = false;
+bananas.onclick = () => {
+  highlight(bananas,bananaName,bananaClicked)
+  if (bananaClicked === true){
+    foodString.push("bananas");
+    sendApiRequest(foodString)
   }else{
-    clearApiData();
+    
+    const index = foodString.indexOf("bananas");
+    if (index > -1) { 
+      foodString.splice(index, 1); 
+    }
+    sendApiRequest(foodString)
   }
     
 }
 
-var beanselement = document.getElementById("beans")
-var beansisClicked = false;
-beanselement.onclick = () => {
-  highlight(beanselement,beansisClicked)
-  sendApiRequest("beans")
+let beans = document.getElementById("beans")
+let beansName = 'beansClicked';
+var beansClicked = false;
+beans.onclick = () => {
+  highlight(beans,beansName,beansClicked)
+  if (beansClicked === true){
+    foodString.push("beans");
+    sendApiRequest(foodString)
+  }else{
+    const index = foodString.indexOf("beans");
+    if (index > -1) { 
+      foodString.splice(index, 1); 
+    }
+    sendApiRequest(foodString)
+  }
+    
 }
 
+//API tester
 let searchButton = document.querySelector("#search")
 
 searchButton.addEventListener("click", ()=>{
@@ -31,7 +56,13 @@ searchButton.addEventListener("click", ()=>{
   sendApiRequest(food)
 })
 
+
+//send API request
 async function sendApiRequest(food){
+  if(food == ""){
+    clearApiData();
+    return
+  }
   let APP_ID = config.APP_ID
   let API_KEY = config.API_KEY
   // let food = document.getElementById('searchTerm').value
@@ -42,6 +73,7 @@ async function sendApiRequest(food){
   useApiData(data)
 }
 
+//replace content div in HTML with API results
 function useApiData(data){
   document.querySelector("#content").innerHTML = `
   <img src="${data.hits[0].recipe.image}">
@@ -49,6 +81,7 @@ function useApiData(data){
   `
 }
 
+//function to clear the content div
 function clearApiData(){
   document.querySelector("#content").innerHTML = ``
 }
